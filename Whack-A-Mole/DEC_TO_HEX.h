789c76c65b525e029925da601e1454f28e6b672c
Gopal Agarwal <18uec115@lnmiit.ac.in>
2021-09-14 15:07:22 +0530

    Create DEC_TO_HEX.h

---
+++ Whack-A-Mole/DEC_TO_HEX.h
@@ -0,0 +1,5 @@
+#ifndef __DEC_TO_HEX_H__                            //IF NOT DEFINE DEFINE MY_HEADER_FILE.H
+#define __DEC_TO_HEX_H__                            //DEFINING MY_HEADER_FILE.H HEADER FILE
+
+unsigned int dec_to_hex(unsigned int index);            //DECLARING PROTOTYPE FOR DEC_TO_HEX FUNCTION PROTOTYPE
+#endif                                                  //ENDING IF
